71f6080757019bf74008d4930012fc4cfd8b86ae
Rashaun Warner <rashaunwarner@yahoo.com>
2026-03-17 15:53:27 -0400

    add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Dashboard from './Dashboard'
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
 
   return (
     <>
       <Dashboard />
+      <Analytics />
     </>
   )
 }
